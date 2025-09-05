9d0ac7efc5510c0c2436ed953a8b85474147caf0
officialnochill <63358288+officialnochill@users.noreply.github.com>
2025-09-05 19:44:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     title: seoData.openGraph.title,
     excerpt: 'home',
     slug: '/',
-    coverImage: 'https://vjy.me/preview.jpg'
+    coverImage: 'https://github.com/officialnochill/hmp/blob/main/public/preview.png'
   };
   return (
     <>
