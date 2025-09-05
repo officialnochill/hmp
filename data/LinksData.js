2faaea5a4c3cfd8273e4317af82e7623786ab429
officialnochill <63358288+officialnochill@users.noreply.github.com>
2025-09-05 19:12:24 +0200

    Update LinksData.js

--- data/LinksData.js
+++ data/LinksData.js
@@ -108,138 +108,11 @@ const webLinks = [
     },
     // Install section
     {
-        title: 'Check out my Showreel!',
+        title: 'Use this template',
         url: 'https://github.com/realvjy/nxt-lnk/generate',
         type: 'install',
         icon: '/use.png',
-        on: true
-    },
-    {
-        title: 'Deploy on Vercel',
-        url: 'https://vercel.com/import/git?s=https://github.com/realvjy/nxt-lnk',
-        type: 'install',
-        icon: '/vercel.svg',
         on: false
-    },
-    {
-        title: 'Documentation',
-        url: 'https://github.com/realvjy/nxt-lnk#nxt-lnk',
-        type: 'install',
-        icon: '/doc.png',im// date 29-Jul, 2022
-const webLinks = [
-    // All social profile
-    {
-        title: 'Twitter',
-        url: 'https://x.com/off_nochill',
-        type: 'social',
-        icon: '/twitter.svg',
-        on: true
-    },
-    {
-        title: 'Instagram',
-        url: 'https://www.instagram.com/off_nochill/',
-        type: 'social',
-        icon: '/insta.svg',
-        on: true
-    },
-    {
-        title: 'Dribbble',
-        url: 'https://dribbble.com/realvjy',
-        type: 'social',
-        icon: '/dribbble.svg',
-        on: false
-    },
-    {
-        title: 'Medium',
-        url: 'https://medium.com/@realvjy',
-        type: 'social',
-        icon: '/medium.svg',
-        on: false
-    },
-    {
-        title: 'Github',
-        url: 'https://github.com/realvjy',
-        type: 'social',
-        icon: '/github.svg',
-        on: false
-    },
-    {
-        title: 'Youtube',
-        url: 'https://www.youtube.com/@officialnochill/',
-        type: 'social',
-        icon: '/youtube.svg',
-        on: true
-    },
-    {
-        title: 'Figma',
-        url: 'https://figma.com/@realvjy',
-        type: 'social',
-        icon: '/figma.svg',
-        on: false
-    },
-    {
-        title: 'Behance',
-        url: 'https://behance.net/realvjy',
-        type: 'social',
-        icon: '/behance.svg',
-        on: false
-    },
-    {
-        title: 'Linkedin',
-        url: 'https://www.linkedin.com/in/realvjy/',
-        type: 'social',
-        icon: '/linkedin.svg',
-        on: false
-    },
-    {
-        title: 'Mastodon',
-        url: 'https://mastodon.online/@realvjy',
-        type: 'social',
-        icon: '/mastodon.svg',
-        on: false
-    },
-    {
-        title: 'Hive Social',
-        url: 'https://www.hivesocial.app/?@realvjy',
-        type: 'social',
-        icon: '/hive.svg',
-        on: false
-    },
-    {
-        title: 'Post',
-        url: 'https://www.post.news/realvjy',
-        type: 'social',
-        icon: '/post.svg',
-        on: false
-    },
-    {
-        title: 'foundation',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/foundation.svg',
-        on: false
-    },
-    {
-        title: 'opensea',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
-        on: false
-    },
-    {
-        title: 'new collection',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
-        on: false
-    },
-    // Install section
-    {
-        title: 'Use this template',
-        url: 'https://youtu.be/w4RsLRdMNjA',
-        type: 'install',
-        icon: '/use.png',
-        on: true
     },
     {
         title: 'Deploy on Vercel',
@@ -253,42 +126,6 @@ const webLinks = [
         url: 'https://github.com/realvjy/nxt-lnk#nxt-lnk',
         type: 'install',
         icon: '/doc.png',
-        on: false
-    },
-    {
-        title: 'Github',
-        url: 'https://github.com/realvjy/nxt-lnk',
-        type: 'install',
-        icon: '/github-fill.svg',
-        on: false
-    },
-
-    {
-        title: 'Check out my Showreel!',
-        url: 'https://youtu.be/w4RsLRdMNjA',
-        type: 'other',
-        icon: '/bmc.png',
-        on: true
-    },
-
-    {
-        title: 'link demo',
-        url: 'https://vjy.me/lnk',
-        type: 'other',
-        icon: '/vjy.png',
-        on: false
-    },
-
-    {
-        title: 'maker website',
-        url: 'https://vjy.me',
-        type: 'other',
-        icon: '/vjy.png',
-        on: false
-    },
-];
-
-export default webLinks;
         on: false
     },
     {
